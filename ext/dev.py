2ac88da98014c250b5df1a47369d9f6b17a920f0
uwidev <btyang24@gmail.com>
2023-11-06 15:57:48 -0800

    Welcome no longer checks if owner

--- ext/dev.py
+++ ext/dev.py
@@ -1,5 +1,9 @@
 """Developer commands for sandbox purposes."""
+import asyncio
 import logging
+import os
+import re
+from collections import defaultdict
 from typing import TYPE_CHECKING
 
 import discord
@@ -42,6 +46,111 @@ class Dev(commands.Cog):
     #         _log.info(f"{after.pending=}")
     #         _log.info(f"{after.flags.started_onboarding=}")
     #         _log.info(f"{after.flags.completed_onboarding=}")
+
+    @commands.group()
+    async def story(self, ctx):
+        pass
+
+    @story.command(name="compile")
+    async def story_compile(self, ctx):
+        """Saves all message in a .txt file as one long message. Also summarizes the contributions.
+        File name will be the same as the channel name.
+        """
+        channel = ctx.channel
+        contributions = 0
+        emoji = re.compile(r"(<a?:[a-zA-Z0-9\_]+:[0-9]+>)")
+        contributors = defaultdict(int)
+
+        async with channel.typing():
+            if not os.path.isdir("story"):
+                os.makedirs("story")
+
+            participants = defaultdict(int)
+            with open(f"story/{channel.name}.txt", mode="w", encoding="utf-8") as file:
+                async for message in channel.history(
+                    limit=None, before=ctx.message, oldest_first=True
+                ):
+                    contributors[message.author.name] += 1
+
+                    file.write(f"{message.content} ")
+                    contributions += 1
+                    participants[message.author] += 1
+
+                with open(
+                    f"story/{channel.name}-contibutors.txt", mode="w", encoding="utf-8"
+                ) as file:
+                    file.write(
+                        f".\n.\n.\n__**Total contributions: {contributions}**__\n"
+                    )
+
+                    i = 0
+                    for item in sorted(
+                        contributors.items(), key=lambda x: x[1], reverse=True
+                    ):
+                        percent = item[1] / contributions
+                        if i < 5:
+                            file.write(f"**{item[0]}: {item[1]} ({percent:.2%})**\n")
+                        else:
+                            file.write(f"{item[0]}: {item[1]} ({percent:.2%})\n")
+                        i += 1
+
+            # msg = await channel.send('🎉 Done compiling 🎉')
+            # await msg.delete(delay=3)
+            await ctx.message.delete()
+
+    @story.command()
+    async def write(self, ctx, file_name):
+        """Given a existing file name from a compiled story, writes the entire story."""
+        await ctx.send(f"```fix\n>>> {file_name} <<<```")
+        async with ctx.channel.typing():
+            with open(f"story/{file_name}.txt", encoding="utf-8") as file:
+                while True:
+                    i = 0
+                    to_append = ""
+                    to_print = ""
+                    eof_reached = 0
+
+                    while i <= 1900 or to_append != " ":
+                        to_append = file.read(1)
+
+                        if not to_append:
+                            # print('>>> reached EOF')
+                            eof_reached = 1
+                            break
+
+                        to_print += to_append
+                        i += 1
+
+                    await ctx.send(to_print)
+                    await asyncio.sleep(2)
+                    if eof_reached == 1:
+                        break
+
+            with open(f"story/{file_name}-contibutors.txt", encoding="utf-8") as file:
+                while True:
+                    i = 0
+                    to_append = ""
+                    to_print = ""
+                    eof_reached = 0
+
+                    while i <= 1900 or to_append != " ":
+                        to_append = file.read(1)
+
+                        if not to_append:
+                            # print('>>> reached EOF')
+                            eof_reached = 1
+                            break
+
+                        to_print += to_append
+                        i += 1
+
+                    # print('going to print {}'.format(to_print))
+                    await ctx.send(to_print)
+                    if eof_reached == 1:
+                        break
+                    await asyncio.sleep(2)
+
+        await ctx.message.delete()
 
 
 async def setup(bot: commands.Bot):
