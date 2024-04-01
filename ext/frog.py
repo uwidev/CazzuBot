d0cdde80eba9686b158e524c55e5bf5aa8507277
usaradark <btyang24@gmail.com>
2024-03-31 20:23:05 -0700

    fix: Nerf frog experience

--- ext/frog.py
+++ ext/frog.py
@@ -27,8 +27,8 @@ _SCOREBOARD_STAMP = "https://cdn.discordapp.com/emojis/752290769712316506.webp?s
 
 
 class _ExpFrog(Enum):
-    NORMAL: int = 60
-    FROZEN: int = 20
+    NORMAL: int = 30
+    FROZEN: int = 15
 
 
 class Frog(commands.Cog):
